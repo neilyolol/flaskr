d94597390b24f1f11fdfb92420ab0f392dbb14af
neilyolol <yckasim00@163.com>
2020-04-02 18:05:55 +0800

    update

--- auth.py
+++ auth.py
@@ -61,3 +61,20 @@ def load_logged_in_user():
     else:
         g.user = get_db().excute('SELECT * FROM user WHERE id = ?', (user_id)).fetchone()
 
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
